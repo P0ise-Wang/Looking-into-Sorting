088a2502d8328c6e3c8497532598042acfaa334d
P0ise-Wang <95836058+P0ise-Wang@users.noreply.github.com>
2022-04-15 19:29:43 +0800

    second commit

---
+++ build/precache-manifest.07c38995f664bd6701eca2fcf00e5051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51599246ce7d4f8b42be2cbb41743b33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc1348e66a5ac24978f6",
+    "url": "./static/css/2.013590b5.chunk.css"
+  },
+  {
+    "revision": "d4fe185284a154ad5786",
+    "url": "./static/css/main.3a338490.chunk.css"
+  },
+  {
+    "revision": "dc1348e66a5ac24978f6",
+    "url": "./static/js/2.98c52b52.chunk.js"
+  },
+  {
+    "revision": "d4fe185284a154ad5786",
+    "url": "./static/js/main.8cd06ce6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
